c399c302515c86da8b17d3c9d6ef794608406d38
Hoon Kim <hoonsubin@icloud.com>
2020-11-05 22:57:27 -0800

    updated packages

--- init-install.sh
+++ init-install.sh
@@ -59,7 +59,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     source ~/.cargo/env
 
     # install nvm
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
     # This loads nvm
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
